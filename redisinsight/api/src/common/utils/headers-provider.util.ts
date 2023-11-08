346f7edc6f8c2247985c6bb3b3b50f2625eb4448
Mateusz Urbański <mateusz.urbanski@bankmillennium.pl>
2023-11-08 00:17:45 +0000

    Add console debugging

--- redisinsight/api/src/common/utils/headers-provider.util.ts
+++ redisinsight/api/src/common/utils/headers-provider.util.ts
@@ -3,6 +3,10 @@ import { lookup } from 'mime-types'
 export function setContentTypeHeaders(res, path, stat) {
     let contentType = lookup(path);
     if(contentType) {
+      console.log("Setting content type of '" + path + "' to '" + contentType + "'");
       res.type(contentType);
+    }
+    else {
+      console.log("Skipping setting content type of '" + path + " because it wasn't recognized.");
     }
   };
